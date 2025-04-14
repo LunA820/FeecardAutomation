496a9592449c868a0a600528b21fa6ddda9b1d4b
LunA820 <lunayang820@gmail.com>
2025-04-15 00:08:15 +1000

    log event to test

--- Lambda-FeecardUpload/lambda_function.py
+++ Lambda-FeecardUpload/lambda_function.py
@@ -1,8 +1,9 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    print("EVENT:", json.dumps(event)) 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! From Github action from Luna')
     }
+   
